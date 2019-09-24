638dc6a1fabf0c00c3467cd292ca055d928111c1
Amaya <Amaya@Amaya-PC>
2019-09-23 19:22:20 -0600

    I made some changes in MainForm. 23-09-2019

---
+++ ChemyColor/src/CapaDatos/ProcedimientosAlmacenados_ChemyProject.sql
@@ -0,0 +1,208 @@
+--Para entrada de factura
+CREATE OR REPLACE PROCEDURE EntraFactura (
+	p_NumFactura VARCHAR2, 
+	p_cont_O_credit NUMBER, 
+	p_CantidadDiasCredit NUMBER, 
+	p_Estado NUMBER, 
+	p_CodigoBodega NUMBER,
+	p_CodigoArticulo NUMBER,
+	p_Fecha DATE, 
+	p_Factura VARCHAR2, 
+	p_CCF VARCHAR2, 
+	p_Ticket VARCHAR2, 
+	p_AjusteInventario VARCHAR2,
+	p_CodigoProveedor NUMBER, 
+	p_eUnidad NUMBER, 
+	p_ePrecio NUMBER, 
+	p_Detalle VARCHAR2)
+AS
+		v_eTotal NUMBER; 
+		v_sUnidad NUMBER; 
+		v_sUnit NUMBER; 
+		v_sUnidadAnterior NUMBER;
+		v_sCostoPromedio NUMBER; 
+		v_sTotal NUMBER;
+		v_sTotalAnterior NUMBER;
+BEGIN
+
+		v_eTotal := (p_eUnidad * p_ePrecio);
+		
+		 SELECT SSUnidad INTO v_sUnidadAnterior FROM (SELECT sUnidad AS SSUnidad FROM HistorialFacturas 
+								WHERE CodigoArticulo = p_CodigoArticulo ORDER BY Codigo DESC) 
+         WHERE ROWNUM <= 1;
+
+		IF (v_sUnidadAnterior IS NULL)
+			THEN
+				v_sUnidadAnterior := 0;
+			END IF;
+
+		v_sUnidad := (v_sUnidadAnterior + p_eUnidad);
+		
+		 SELECT SSTotal INTO v_sTotalAnterior FROM (SELECT sTotal AS SSTotal FROM HistorialFacturas 
+								WHERE	CodigoArticulo = p_CodigoArticulo ORDER BY Codigo DESC) 
+         WHERE ROWNUM <= 1;
+
+		IF (v_sTotalAnterior IS NULL)
+			THEN
+				v_sTotalAnterior := 0;
+        END IF;
+
+		v_sCostoPromedio := ((v_eTotal + v_sTotalAnterior) / v_sUnidad);
+	
+		v_sTotal := (v_sUnidad * v_sCostoPromedio);
+
+		INSERT INTO HistorialFacturas 
+			(Codigo,NumFactura, cont_O_credit, CantidadDiasCredit, Estado, CodigoBodega, 
+			CodigoArticulo, Fecha, Factura, CCF, Ticket, AjusteInventario, CodigoProveedor, eUnidad, ePrecio, eTotal, 
+			sUnidad, sCostoPromedio, sTotal, Detalle) 
+	
+		VALUES 
+			(HistorialFacturaSeq.nextval, p_NumFactura, p_cont_O_credit, p_CantidadDiasCredit, p_Estado, p_CodigoBodega, p_CodigoArticulo, p_Fecha, p_Factura,
+			p_CCF, p_Ticket, p_AjusteInventario, p_CodigoProveedor, p_eUnidad, p_ePrecio, v_eTotal, v_sUnidad, v_sCostoPromedio, 
+			v_sTotal, p_Detalle);
+
+		UPDATE Articulos SET Existencia = Existencia + p_eUnidad WHERE CodigoArticulo = p_CodigoArticulo;
+END;
+/
+
+
+
+
+--Salida factura
+CREATE OR REPLACE PROCEDURE SalidaFactura (
+	p_NumFactura VARCHAR2,
+	p_Fecha DATE,
+	p_cont_O_credit NUMBER,
+	p_CantidadDiasCredit NUMBER,
+	p_oCliente VARCHAR2,
+	p_CodigoArticulo NUMBER,	
+	p_oUnidad NUMBER,
+	p_CodigoBodega NUMBER)
+AS
+			v_oPrecio NUMBER;
+			v_oTotal NUMBER;
+			v_sUnidad NUMBER; 
+			v_sTotal NUMBER;
+			v_CCF VARCHAR2(50);
+			v_Estado NUMBER;
+			v_sCostoPromedio NUMBER;
+	BEGIN
+
+		 SELECT ssCostoPromedio INTO v_sCostoPromedio FROM (SELECT sCostoPromedio AS ssCostoPromedio FROM HistorialFacturas 
+								WHERE CodigoArticulo = p_CodigoArticulo ORDER BY Codigo DESC) 
+        WHERE ROWNUM <= 1;
+
+		IF (v_sCostoPromedio IS NULL)
+			THEN
+				v_sCostoPromedio := 0;
+			END IF;
+		
+		v_Estado := 1;
+
+		v_oPrecio := v_sCostoPromedio;
+
+		v_oTotal := (v_oPrecio * p_oUnidad);
+
+		SELECT ssUnidad INTO v_sUnidad FROM (SELECT sUnidad AS ssUnidad FROM HistorialFacturas WHERE CodigoArticulo = p_CodigoArticulo 
+						ORDER BY Codigo DESC) 
+        WHERE ROWNUM <= 1;
+        
+        v_sUnidad := v_sUnidad - p_oUnidad;
+		
+		v_sTotal := (v_sUnidad * v_sCostoPromedio);
+
+		v_CCF := p_NumFactura;
+
+		INSERT INTO HistorialFacturas 
+			(Codigo, NumFactura, cont_O_credit, CantidadDiasCredit, Estado,CodigoBodega, CodigoArticulo,Fecha, CCF, 
+			oUnidad, oPrecio, oTotal, oCliente, sUnidad, sCostoPromedio, sTotal)
+		VALUES 
+			(HistorialFacturaSeq.nextval, p_NumFactura, p_cont_O_credit, p_CantidadDiasCredit, v_Estado, p_CodigoBodega, p_CodigoArticulo, p_Fecha,
+			v_CCF, p_oUnidad, v_oPrecio, v_oTotal, p_oCliente, v_sUnidad, v_sCostoPromedio, v_sTotal);
+
+		UPDATE Articulos SET Existencia = Existencia - p_oUnidad WHERE CodigoArticulo = p_CodigoArticulo;
+	END;
+/
+
+
+--Para ajuste individual
+CREATE OR REPLACE PROCEDURE AjusteInventarioIndividual (
+	p_Fecha DATE,
+	p_CodigoArticulo NUMBER,	
+	p_oUnidad NUMBER,
+	p_CodigoBodega NUMBER)
+AS
+			v_oPrecio NUMBER;
+			v_oTotal NUMBER;
+			v_sUnidad NUMBER; 
+			v_sTotal NUMBER;
+			v_CCF VARCHAR2(50);
+			v_Estado NUMBER;
+			v_sCostoPromedio NUMBER;
+	BEGIN
+
+		 SELECT ssCostoPromedio INTO v_sCostoPromedio FROM (SELECT sCostoPromedio AS ssCostoPromedio FROM HistorialFacturas WHERE CodigoArticulo = p_CodigoArticulo ORDER BY Codigo DESC) 
+         WHERE ROWNUM <= 1;
+
+		 SELECT ooPrecio INTO v_oPrecio  FROM (SELECT oPrecio AS ooPrecio FROM HistorialFacturas WHERE CodigoArticulo = p_CodigoArticulo ORDER BY Codigo DESC) 
+         WHERE ROWNUM <= 1;
+
+		v_oTotal := (v_oPrecio * p_oUnidad);
+
+		SELECT ssUnidad INTO v_sUnidad FROM (SELECT sUnidad AS ssUnidad FROM HistorialFacturas WHERE CodigoArticulo = p_CodigoArticulo ORDER BY Codigo DESC) 
+        WHERE ROWNUM <= 1;
+        
+        v_sUnidad := v_sUnidad - p_oUnidad;
+		
+		v_sTotal := (v_sUnidad * v_sCostoPromedio);
+
+		INSERT INTO HistorialFacturas 
+			(Codigo, NumFactura, cont_O_credit, CantidadDiasCredit, Estado,CodigoBodega, CodigoArticulo,Fecha, CCF, 
+			oUnidad, oPrecio, oTotal, oCliente, sUnidad, sCostoPromedio, sTotal, AjusteInventario, Factura, Ticket)
+		VALUES 
+			(HistorialFacturaSeq.nextval,'---', 0, 0, 1, p_CodigoBodega, p_CodigoArticulo, p_Fecha,
+			'---', p_oUnidad, v_oPrecio, v_oTotal, '---', v_sUnidad, v_sCostoPromedio, v_sTotal, 'Ajuste', '---', '---');
+
+		UPDATE Articulos SET Existencia = Existencia - p_oUnidad WHERE CodigoArticulo = p_CodigoArticulo;
+	END;
+/
+
+
+--Para Ajuste de inventario anual
+CREATE OR REPLACE PROCEDURE AjusteInventario (
+    p_Fecha DATE,
+    p_CodigoArticulo NUMBER)
+AS
+		v_sUnidad NUMBER; 
+		v_sCostoPromedio NUMBER; 
+		v_sTotal NUMBER;
+BEGIN
+
+		SELECT ssUnidad, ssCostoPromedio, ssTotal INTO v_sUnidad, v_sCostoPromedio, v_sTotal 
+        FROM (SELECT sUnidad AS ssUnidad, sCostoPromedio AS ssCostoPromedio, sTotal AS ssTotal 
+              FROM HistorialFacturas
+              WHERE CodigoArticulo = p_CodigoArticulo
+              ORDER BY Codigo DESC) 
+        WHERE ROWNUM <= 1;
+
+		INSERT INTO HistorialFacturas (Codigo, NumFactura, Cont_o_Credit, CantidadDiasCredit, Estado, CodigoBodega, 
+                    CodigoArticulo, Fecha, Factura, CCF, Ticket, AjusteInventario, CodigoProveedor, eUnidad, ePrecio, eTotal, 
+                    oUnidad, oPrecio, oTotal, oCliente, sUnidad, sCostoPromedio, sTotal, Detalle)
+		VALUES
+			(HistorialFacturaSeq.nextval, '',0,0,0,NULL,p_CodigoArticulo, p_Fecha,NULL,NULL,NULL,'1',NULL,NULL,NULL,NULL,NULL, NULL, 
+			NULL,NULL,v_sUnidad,v_sCostoPromedio,v_sTotal,NULL);
+END;
+/
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ChemyColor/src/CapaDatos/Script-ChemyColor.sql
@@ -0,0 +1,339 @@
+create table TipoContribuyente 
+(
+CodigoTipoContribuyente int primary key,
+TipoContribuyente varchar(100)
+);
+
+create sequence CodigoTipoContribuyente1 minvalue 1 start with 1 cache 10;
+
+insert into TipoContribuyente values ( CodigoTipoContribuyente1.nextval ,'Grande');
+insert into TipoContribuyente values (CodigoTipoContribuyente1.nextval ,'Mediana');
+insert into TipoContribuyente values (CodigoTipoContribuyente1.nextval ,'Pequeña');
+
+
+create table TipoProveedores
+(
+CodTipoProv int primary key,
+TipoProv varchar(50)
+);
+
+create sequence CodTipoProv minvalue 1 start with 1 cache 10;
+
+create table Proveedores
+(
+CodigoProveedor int primary key,
+Nombres varchar(100) UNIQUE,
+NRC  varchar(100),
+FechaIngreso varchar(12),
+Saldo decimal(18,4),
+Direccion varchar(600),
+Email varchar(50),
+Registro varchar(20),
+Nit varchar(20),
+DUI varchar(10),
+Giro varchar(20),
+Limite varchar(20),
+CuentaPorPagar varchar(100),
+CodigoTipoContribuyente int, foreign key (CodigoTipoContribuyente) references TipoContribuyente(CodigoTipoContribuyente),
+CodTipoProv int, foreign key (CodTipoProv) references TipoProveedores(CodTipoProv)
+);
+
+create sequence CodigoProveedor minvalue 1 start with 1 cache 10;
+
+create table TelefonosProveedor
+(
+CodigoProveedor int, foreign key (CodigoProveedor) references Proveedores(CodigoProveedor),
+Telefono varchar(12)
+);
+
+
+create table Bodegas
+(
+CodigoBodega int primary key,
+NombreBodega varchar(30) unique not null,
+Direccion varchar(300)not null,
+Email varchar(50) unique not null,
+Encargado varchar(50) not null,
+DarCredito integer,
+PModPrec integer,
+PFacSinExis integer,
+DescMaxPorVend varchar(20),
+SerieComprobante varchar(50),
+DescMaxPermitido varchar(20),
+Multicaja integer,
+ImpRecAb integer,
+Estado integer
+);
+
+create sequence CodigoBodega minvalue 1 start with 1 cache 10;
+
+create table TelefonosPorBodega
+(
+CodigoBodega int, foreign key (CodigoBodega) references Bodegas(CodigoBodega),
+Telefono varchar(15),
+Orden int
+);
+
+create table TiposFacturas
+(
+CodigoTipoFact int primary key,
+TipoFactura varchar(50)
+);
+
+create sequence CodigoTipoFact minvalue 1 start with 1 cache 10;
+
+create table FacturacionBodega
+(
+CodigoBodega int, foreign key (CodigoBodega) references Bodegas(CodigoBodega),
+CodigoTipoFact int, foreign key (CodigoTipoFact) references TiposFacturas(CodigoTipoFact)
+);
+
+create table FormasPago
+(
+CodigoFormaPago int primary key,
+FormaPago varchar(50)
+);
+
+create sequence CodigoFormaPago minvalue 1 start with 1 cache 10;
+
+create table FormasPagoPorBodega
+(
+CodigoBodega int, foreign key (CodigoBodega)  references Bodegas(CodigoBodega),
+CodigoFormaPago int, foreign key (CodigoFormaPago) references FormasPago(CodigoFormaPago)
+);
+
+create table TiposMovimientos
+(
+CodTipoMov int primary key,
+TipoMov varchar(50)
+);
+
+create sequence CodTipoMov minvalue 1 start with 1 cache 10;
+
+create table IVA
+(
+CodigoIVA int primary key,
+ValorIVA varchar(10)
+);
+
+create sequence CodigoIVA minvalue 1 start with 1 cache 10;
+
+create table FacturasCompras
+(
+CodFactCompra int primary key,
+Fecha varchar(12),
+CodigoProveedor int, foreign key (CodigoProveedor)references Proveedores(CodigoProveedor),
+NumeroComprobante varchar(100),
+RegistroMes varchar(12),
+RegistroAnnio varchar(12),
+CodigoFormaPago int, foreign key (CodigoFormaPago) references FormasPago(CodigoFormaPago),
+CodTipoMov int, foreign key (CodTipoMov) references TiposMovimientos(CodTipoMov),
+NoRem varchar(100),
+Observaciones varchar(300),
+CodigoBodega int, foreign key (CodigoBodega) references Bodegas(CodigoBodega),
+CodigoIVA int, foreign key (CodigoIva) references IVA(CodigoIva)
+);
+
+
+create table Grupos
+(
+CodigoGrupo int primary key,
+NombreGrupo varchar(50),
+Comision decimal(18,4)
+);
+
+
+create table Lineas
+(
+CodigoLinea int primary key,
+CodigoGrupo int, foreign key (CodigoGrupo) references Grupos(CodigoGrupo),
+NombreLineas varchar(50)
+);
+
+create table Articulos
+(
+CodigoArticulo int primary key,
+codigoProductos varchar(100) not null,
+NombreArticulo varchar(50) not null,
+CodigoBarra varchar(25) not null,
+ExistenciaMin int,
+CodigoGrupo int, foreign key (CodigoGrupo) references Grupos(CodigoGrupo),
+CodigoBodega int, foreign key (CodigoBodega) references Bodegas(CodigoBodega),
+CodigoLinea int, foreign key (CodigoLinea) references Lineas(CodigoLinea),
+Existencia int,
+Utilidad decimal(18,4)
+);
+
+create table UnidadesMedida
+(
+CodigoArticulo int, foreign key (CodigoArticulo) references Articulos(CodigoArticulo),
+UnidadMedida varchar(50)
+);
+
+
+create table PrecioPorArticulo
+(
+CodigoArticulo int, foreign key (CodigoArticulo) references Articulos(CodigoArticulo),
+cantidad int,
+Precio decimal(18,4)
+);
+
+create table DetalleFacturaCompra
+(
+CodFactCompra int, foreign key (CodFactCompra) references FacturasCompras(CodFactCompra),
+Item int,
+CodigoArticulo int, foreign key (CodigoArticulo) references Articulos(CodigoArticulo),
+Descripcion varchar(200),
+Cantidad int, 
+Descuento varchar(20),
+Valor decimal(18,4),
+CuentaContable varchar(20)
+);
+
+
+create table TipoUsuarios
+(
+CodTipoUsuario int primary key,
+TipoUsuario varchar(50) not null
+);
+
+create sequence SeqCodTipoUsuario 
+maxvalue 99999999999
+start with 1
+minvalue 1  
+cache 10;
+
+insert into TipoUsuarios values (CodTipoUsuario.nextval,'Administrador');
+insert into TipoUsuarios values (CodTipoUsuario.nextval,'Jefe de Bodega');
+insert into TipoUsuarios values (CodTipoUsuario.nextval,'Auxiliar de Bodega');
+
+
+
+create table Usuarios
+(
+CodigoUsuario int primary key,
+CodTipoUsuario int, foreign key (CodTipoUsuario) references TipoUsuarios(CodTipoUsuario),
+NombreUsuario varchar(100) not null,
+ApellidoUsuario varchar(100) not null,
+Email varchar(100),
+Usuario varchar(50) not null unique,
+Contrasena varchar(100) not null,
+Celular varchar(10),
+Direccion varchar(300),
+Estado integer
+);
+
+--Para poner un usuario por default.
+INSERT INTO 
+	Usuarios 
+VALUES 
+	(1, 'ADMIN','ADMIN', 'admin@chemycolor.com', 'admin', 
+	0x07E6E506A23ABD3E2F0205567EC06CA74D9E45B67D0A32AAF980A8182A91F67B,
+	0x1BD5131FCEF59197DFBB8FB4D9523CFEB0864A4B523BBA8FDF0A8552F1284EA5, '0000-0000', 'Chemy',1);
+
+
+
+
+--David 16-05-2019
+CREATE TABLE HistorialFacturas
+(
+Codigo INT PRIMARY KEY,
+NumFactura VARCHAR(25) NOT NULL,
+cont_O_credit integer NOT NULL, --Variable para saber si es al contado o al credito. Contado = true y Credito = false
+CantidadDiasCredit INT, --Variable para saber la cantidad de los dias si es al crédito
+Estado integer NOT NULL, --Variable para saber si es entrada o salida. Entrada = false(0) y Salida = true(1)
+CodigoBodega INT, FOREIGN KEY (CodigoBodega) REFERENCES Bodegas(CodigoBodega),
+CodigoArticulo INT, FOREIGN KEY (CodigoArticulo) REFERENCES Articulos(CodigoArticulo),
+Fecha date,
+Factura VARCHAR(50),
+CCF VARCHAR(50),
+Ticket VARCHAR(50),
+AjusteInventario VARCHAR(100),
+CodigoProveedor INT, FOREIGN KEY (CodigoProveedor) REFERENCES Proveedores(CodigoProveedor),
+
+--entradas simbología = e
+eUnidad INT,
+ePrecio decimal(18,4),
+eTotal decimal(18,4),
+
+--salidas simbología = o (Out en ingles)
+oUnidad INT,
+oPrecio decimal(18,4),
+oTotal decimal(18,4),
+oCliente VARCHAR(100),
+
+--Saldos simbología = s
+sUnidad INT,
+sCostoPromedio decimal(18,4),
+sTotal decimal(18,4),
+Detalle VARCHAR(1024)
+);
+
+
+
+CREATE TABLE DetallesFactura 
+(
+	Codigo INT, FOREIGN KEY (Codigo) REFERENCES HistorialFacturas(Codigo),
+	Nit VARCHAR(25),
+	NRC VARCHAR(50),
+	Giro VARCHAR(50),
+	Dui VARCHAR(25),
+	Direccion VARCHAR(1024),
+	IvaCreditoFiscal VARCHAR(25)
+);
+
+
+
+
+
+
+--04-06-2019 DAVID
+
+CREATE TABLE UsuarioSesion
+(
+	Codigo INT PRIMARY KEY,
+	Nombre VARCHAR(50)
+);
+
+
+INSERT INTO UsuarioSesion VALUES ('admin');
+
+
+CREATE TABLE EmailRecuperador
+(
+	Codigo INT PRIMARY KEY,
+	Email VARCHAR(100),
+	Contrasennia VARCHAR(50)
+);
+
+
+INSERT INTO 
+	EmailRecuperador 
+VALUES 
+	('prueba@chemy.com','admin');
+GO
+
+
+
+
+
+--Secuencias Septiembre 2019
+
+CREATE SEQUENCE HistorialFacturaSeq
+MINVALUE 1
+MAXVALUE 9999999999999
+START WITH 1
+INCREMENT BY 1
+CACHE 20;
+
+CREATE SEQUENCE AjusteInventarioSeq
+MINVALUE 1
+MAXVALUE 9999999999999
+START WITH 1
+INCREMENT BY 1
+CACHE 20;
+
+
+
+
+
